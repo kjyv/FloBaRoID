24d659449ec0ad14d3e4cbdb8a747ef7493fcf6a
Stefan <stefan.bethge@iit.it>
2016-02-11 18:09:40 +0100

    send velocities as well to use for other controllers
    return measurements directly after setting setpoints

--- excitation/src/excitation_thread.cpp
+++ excitation/src/excitation_thread.cpp
@@ -22,24 +22,25 @@ excitation_thread::excitation_thread(   std::string module_prefix,
                                             generic_thread( module_prefix, rf, ph )
 {
     // position mode on left arm chain interface
-    left_arm_chain_interface.setPositionDirectMode();
-//            setPositionMode();
+    left_arm_chain_interface.setPositionDirectMode();   //sets setpoint directly, uses maximum speed all the time
+    //left_arm_chain_interface.setPositionMode();   //generate trajectories, allows setting velocities
+    //left_arm_chain_interface.setVelocityMode();
 
     //open data output port
-    outgoingPort.open("/" + module_prefix + "/dataOutput:o");
+    outgoingPort.open("/" + module_prefix + "/state:o");
 }
 
 excitation_thread::~excitation_thread()
 {
-    save(&tmpLog, "log.csv");
+    //save(&tmpLog, "log.csv");
 }
 
 void excitation_thread::link_tutorial_params()
 {
     // get a shared pointer to param helper
     std::shared_ptr<paramHelp::ParamHelperServer> ph = get_param_helper();
-    // link the max_vel parameter (single value linking)
-    ph->linkParam( PARAM_ID_MAX_VEL, &max_vel );
+    // link parameters (single value linking)
+    //ph->linkParam( PARAM_ID_N_DOFS, &n_dofs );
 }
 
 bool excitation_thread::custom_init()
@@ -53,7 +54,9 @@ bool excitation_thread::custom_init()
 void excitation_thread::run()
 {
     static yarp::sig::Vector pos;
-    if(pos.size()!=N_DOFS) pos.resize(N_DOFS);
+    if(pos.size()!=num_joints) pos.resize(num_joints);
+    static yarp::sig::Vector vels;
+    if(vels.size()!=num_joints) vels.resize(num_joints);
 
     static yarp::sig::Vector q_left_arm, qdot_left_arm, tau_left_arm;
 
@@ -63,7 +66,7 @@ void excitation_thread::run()
     double l=1, w_f=1, a=0.5, b=0.8, q0=-35, nf=1;
     double angle = (a/(w_f*l)*sin(w_f*l*t) +  a/(w_f*l)*cos(w_f*l*t))/M_PI*180;
 
-    if(pos.size()!=N_DOFS) pos.resize(N_DOFS);
+    if(pos.size()!=num_joints) pos.resize(num_joints);
     pos.zero();
     /*pos[0] = excitation_cmd.angle0;
     pos[1] = excitation_cmd.angle1;
@@ -88,8 +91,21 @@ void excitation_thread::run()
         pos[4] = excitation_cmd.angle4;
         pos[5] = excitation_cmd.angle5;
         pos[6] = excitation_cmd.angle6;
+
+        vels[0] = excitation_cmd.velocity0;
+        vels[1] = excitation_cmd.velocity1;
+        vels[2] = excitation_cmd.velocity2;
+        vels[3] = excitation_cmd.velocity3;
+        vels[4] = excitation_cmd.velocity4;
+        vels[5] = excitation_cmd.velocity5;
+        vels[6] = excitation_cmd.velocity6;
+
+        left_arm_chain_interface.setReferenceSpeeds(vels);
         left_arm_chain_interface.move(pos);
 
+        //left_arm_chain_interface.move(vels);
+
+        // get state data and send
         left_arm_chain_interface.sensePosition(q_left_arm);     //get positions in deg
         left_arm_chain_interface.senseVelocity(qdot_left_arm);  //get velocities in deg/s
         left_arm_chain_interface.senseTorque(tau_left_arm);     //get torques in Nm
@@ -106,9 +122,12 @@ void excitation_thread::run()
         }
         out_bottle.addDouble(t);
 
-        command_interface.command_port.write(out_bottle);
+        if(outgoingPort.isOpen()) {
+            outgoingPort.write(out_bottle);
+        }
     }
-    else if( excitation_cmd.command == "get_left_arm_measurements" ) {
+
+    /*else if( excitation_cmd.command == "get_left_arm_measurements" ) {
         left_arm_chain_interface.sensePosition(q_left_arm);     //get positions in deg
         left_arm_chain_interface.senseVelocity(qdot_left_arm);  //get velocities in deg/s
         left_arm_chain_interface.senseTorque(tau_left_arm);     //get torques in Nm
@@ -130,30 +149,19 @@ void excitation_thread::run()
         }
 
         yarp::sig::Vector out;
-        out.resize(N_DOFS*3+1);
+        out.resize(num_joints*3+1);
         out.setSubvector(0, q_left_arm);
-        out.setSubvector(N_DOFS, qdot_left_arm);
-        out.setSubvector(N_DOFS*2, tau_left_arm);
-        out[N_DOFS*3] = t;
+        out.setSubvector(num_joints, qdot_left_arm);
+        out.setSubvector(num_joints*2, tau_left_arm);
+        out[num_joints*3] = t;
 
         static VectorXd tmp(out.size());
         for (int i=0; i<tmp.rows(); i++)
           tmp(i) = out(i);
         tmpLog.push_back(tmp);        
-    }
+    }*/
+
     else if( excitation_cmd.command != "" ) {
         std::cout << excitation_cmd.command <<  " -> command not valid" << std::endl;
     }
-}
-
-bool excitation_thread::custom_pause()
-{
-    // set the ref speed to 0 for all the joints
-    left_arm_chain_interface.setReferenceSpeed( 0 );
-}
-
-bool excitation_thread::custom_resume()
-{
-    // set the ref speed to max_vel for all the joints
-    left_arm_chain_interface.setReferenceSpeed( max_vel );
 }

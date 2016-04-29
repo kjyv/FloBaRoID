e87bd413eece402a8fb71694f2a9d2fc3dc6fb9f
Stefan Bethge <stefan.bethge@iit.it>
2016-04-29 11:39:52 +0200

    save random regressor, allow saving identified urdf

--- identification/createNoisyURDF.py
+++ identification/createNoisyURDF.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2.7
 
 import numpy as np
 import iDynTree; iDynTree.init_helpers(); iDynTree.init_numpy_helpers()
@@ -8,6 +9,8 @@ def main():
     parser = argparse.ArgumentParser(description='Load measurements and URDF model to get inertial parameters.')
     parser.add_argument('--urdf_input', required=True, type=str, help='the file to load the robot model from')
     parser.add_argument('--urdf_output', required=True, type=str, help='the file to save the noisy robot model to')
+    parser.add_argument('--noise', required=False, type=float, help='scale of noise (default 0.01)')
+    parser.set_defaults(noise=0.01)
     args = parser.parse_args()
 
     model = iDynTree.Model()
@@ -22,11 +25,11 @@ def main():
     xStdModel = iDynTree.VectorDynSize(n_params)
     dynComp.getModelDynamicsParameters(xStdModel)
     xStdModel = xStdModel.toNumPy()
-    xStdModel += np.random.randn(n_params)*0.01
+    xStdModel += np.random.randn(n_params)*args.noise
 
     helpers = identificationHelpers.IdentificationHelpers(n_params)
     helpers.replaceParamsInURDF(input_urdf=args.urdf_input, output_urdf=args.urdf_output, \
-                                params=xStdModel, link_names=link_names)
+                                new_params=xStdModel, link_names=link_names)
 
 if __name__ == '__main__':
     main()

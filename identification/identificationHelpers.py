74108cb6e840a84add16938ee75b4291b09cd815
Stefan Bethge <stefan.bethge@iit.it>
2016-05-30 19:09:59 +0200

    try constrained least squares for base params

--- identification/identificationHelpers.py
+++ identification/identificationHelpers.py
@@ -1,6 +1,7 @@
 import time
 from IPython import embed
 import iDynTree
+import numpy as np
 
 class Timer(object):
     def __enter__(self):
@@ -14,6 +15,36 @@ class Timer(object):
 class IdentificationHelpers(object):
     def __init__(self, n_params):
         self.n_params = n_params
+
+    def bott_duffin(M, v):
+        """
+        Compute a constrained generalized inverse.
+        Specifically, this is the Bott-Duffin inverse of M
+        constrained to the orthogonal complement of v.
+        This function assumes that v has rank 1,
+        although Bott-Duffin inverses are also defined
+        for inverses constrained to orthogonal complements
+        of higher dimensional subspaces.
+        @param M: a matrix
+        @param v: a vector
+        @return: the constrained generalized inverse of M
+        """
+        # check the shapes of the input matrix and vector
+        n = len(v)
+        if M.shape != (n, n):
+            raise ValueError('M and v have incompatible shapes')
+        # check that v is nonzero
+        v_dot_v = np.inner(v, v)
+        if not v_dot_v:
+            raise ValueError('expected nonzero v')
+        # compute the orthogonal projection onto v
+        P = np.outer(v, v) / v_dot_v
+        # compute the orthogonal projection onto the orthogonal complement of v
+        I = np.eye(n)
+        C = I - P
+        # compute the constrained generalized inverse
+        B = np.dot(C, np.linalg.inv(np.dot(M, C) + P))
+        return B
 
     def checkPhysicalConsistency(self, params):
         """check params for physical consistency, expect params relative to link frame"""

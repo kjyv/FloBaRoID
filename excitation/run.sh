d943485442753e7f59f9a4ed26558f0ac00bcedf
Stefan Bethge <stefan.bethge@iit.it>
2016-06-06 19:08:47 +0200

    plot and use raw data fields as well

--- excitation/run.sh
+++ excitation/run.sh
@@ -1,2 +1,2 @@
 #./excite.py --model ../model/bigman_left_arm.urdf --plot --periods=3 --yarp
-./excite.py --model ../model/kuka_lwr4.urdf --plot --periods=3 --ros
+./excite.py --model ../model/kuka_lwr4.urdf --plot --periods=3 --ros --simulate

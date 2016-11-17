f64f45198c2c3bf89bd54fad33089c8a68b3ac02
Stefan Bethge <12260+kjyv@users.noreply.github.com>
2016-11-17 16:16:54 +0100

    output test result in case of failure

--- tests/test_identification.py
+++ tests/test_identification.py
@@ -4,8 +4,11 @@ import os
 
 def test_identification():
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
-    output = subprocess.check_output('./examples/identify_kuka_lwr4.sh', cwd=path, shell=True)
-    print(output)
+    try:
+        output = subprocess.check_output('./examples/identify_kuka_lwr4.sh', cwd=path, shell=True)
+    except subprocess.CalledProcessError as e:
+        print (e.output)
+        return (e.returncode)
 
 if __name__ == '__main__':
     test_identification()

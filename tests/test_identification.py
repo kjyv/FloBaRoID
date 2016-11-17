d6eaf26ba1e18611b0cc0cc68f24ae0a4316cf25
Stefan Bethge <stefan+github@lanpartei.de>
2016-11-17 16:42:50 +0100

    copy shell environment for testing within virtualenv

--- tests/test_identification.py
+++ tests/test_identification.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
 
 import subprocess
 import os
@@ -5,7 +7,7 @@ import os
 def test_identification():
     path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
     try:
-        output = subprocess.check_output('./examples/identify_kuka_lwr4.sh', cwd=path, shell=True)
+        output = subprocess.check_output('./examples/identify_kuka_lwr4.sh', cwd=path, shell=True, env=os.environ.copy())
     except subprocess.CalledProcessError as e:
         print (e.output)
         return (e.returncode)

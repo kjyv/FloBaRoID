use sign(dq) for Coulomb friction regressor, fixes

identification/model.py
@@ -300,7 +300,8 @@ class Model(object):
 
                 # append unitary matrix to regressor for offsets/dry friction
                 if self.opt['identifyTorqueOffsets'] or self.opt['identifyFriction']:
-                    offset_regressor = np.vstack( (np.zeros((fb, self.N_DOFS)), np.identity(self.N_DOFS)))
+                    static_diag = np.identity(self.N_DOFS)*np.sign(dq.toNumPy())
+                    offset_regressor = np.vstack( (np.zeros((fb, self.N_DOFS)), static_diag))
                     regressor = np.concatenate((regressor, offset_regressor), axis=1)
 
                 # append positive/negative velocity matrix for velocity dependent asymmetrical friction
@@ -484,7 +485,8 @@ class Model(object):
 
                 # append unitary matrix to regressor for offsets/dry friction
                 if self.opt['identifyTorqueOffsets'] or self.opt['identifyFriction']:
-                    offset_regressor = np.vstack( (np.zeros((fb*6, self.N_DOFS)), np.identity(self.N_DOFS)))
+                    static_diag = np.identity(self.N_DOFS)*np.sign(dq.toNumPy())
+                    offset_regressor = np.vstack( (np.zeros((fb*6, self.N_DOFS)), static_diag))
                     A = np.concatenate((A, offset_regressor), axis=1)
 
                 # append positive/negative velocity matrix for velocity dependent asymmetrical friction
fix some walkman importer signs, fix output of "real" params

excitation/csv2npz.py
@@ -117,9 +117,9 @@ def readWalkmanCSV(dir, config, plot):
                             1, 1, 1, 1, -1, -1,      #RHipLat -
                             #1,                       #WaistLat
                             1, 1,                     #WaistSag -
-                            1, 1, -1, 1, 1, -1, -1,   #LShSag -
+                            1, -1, -1, 1, 1, -1, -1,   #LShSag -
                             #1, 1,                     #NeckYawj -
-                            -1, 1, -1, -1, 1, 1, -1  #RShSag -
+                            1, -1, -1, -1, 1, 1, 1  #RShSag -
                            ])
     joint_offsets = np.array([0, 0, 0, 0, 0, 0,
                               0, 0, 0, 0, 0, 0,

identification/output.py
@@ -70,11 +70,13 @@ class OutputConsole(object):
             dc = iDynTree.DynamicsRegressorGenerator()
             if not dc.loadRobotAndSensorsModelFromFile(idf.urdf_file_real):
                 sys.exit()
-            tmp = iDynTree.VectorDynSize(idf.model.num_params)
+            tmp = iDynTree.VectorDynSize(idf.model.num_inertial_params)
             #set regressor, otherwise getModelParameters segfaults
             dc.loadRegressorStructureFromString(idf.model.regrXml)
             dc.getModelParameters(tmp)
             xStdReal = tmp.toNumPy()
+            #add some zeros for friction etc.
+            xStdReal = np.concatenate((xStdReal, np.zeros(idf.model.num_params-idf.model.num_inertial_params)))
             xBaseReal = np.dot(idf.model.Binv, xStdReal)
 
         if idf.opt['showStandardParams']:
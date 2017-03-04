62ad081099a2f2945bce84e4ec751cee11f938ac
Stefan Bethge <stefan+github@lanpartei.de>
2017-03-04 20:22:26 +0100

    fix tests

--- examples/identify_kuka_lwr4.sh
+++ examples/identify_kuka_lwr4.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./identification.py --config configs/kuka_lwr4.yaml --model model/kuka_lwr4.urdf --measurements data/KUKA/HW/measurements_3.npz
+./identify.py --config configs/kuka_lwr4.yaml --model model/kuka_lwr4.urdf --measurements data/KUKA/HW/measurements_3.npz

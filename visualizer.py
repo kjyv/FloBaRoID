16f73bddca2b4fa6b5ef4109b44e1e5216da7cd1
Stefan Bethge <12260+kjyv@users.noreply.github.com>
2017-03-12 17:21:50 +0100

    fixes for static optimization (and less constraints)

--- visualizer.py
+++ visualizer.py
@@ -12,7 +12,7 @@ import numpy.linalg as la
 import math
 import collections
 
-import sys
+import os, sys
 import time
 
 from OpenGL import GLU
@@ -32,14 +32,14 @@ def glvecf(v):
 class Cube(object):
     ''' vertices for a cube of size 1 '''
     def __init__(self):
-        self.vertices = np.array([-0.5,  0.5, 0.0,
-                                  -0.5, -0.5, 0.0,
-                                   0.5, -0.5, 0.0,
-                                   0.5,  0.5, 0.0,
-                                  -0.5,  0.5, -1.0,
-                                  -0.5, -0.5, -1.0,
-                                   0.5, -0.5, -1.0,
-                                   0.5,  0.5, -1.0], np.float32)
+        self.vertices = np.array([-0.5,  0.5, 0.5,
+                                  -0.5, -0.5, 0.5,
+                                   0.5, -0.5, 0.5,
+                                   0.5,  0.5, 0.5,
+                                  -0.5,  0.5, -0.5,
+                                  -0.5, -0.5, -0.5,
+                                   0.5, -0.5, -0.5,
+                                   0.5,  0.5, -0.5], np.float32)
         self.indices = np.array([0, 1, 2,
                                  0, 2, 3,
                                  0, 3, 7,
@@ -221,21 +221,27 @@ class FirstPersonCamera(object):
 
 
 class Visualizer(object):
-    def __init__(self):
+    def __init__(self, config):
+        # type: (Dict[str]) -> None
         # some vars
         #self.pressed_keys = []   # type: List[Any]
         self.program = None   # type: List[Any]
         self.window_closed = False
         self.mode = 'b'  # 'b' - blocking or 'c' - continous
+        self.display_index = 0   # current index for displaying e.g. postures from file
+        self.config = config
 
         # keep a list of bodies
         self.bodies = []     # type: List[Dict[str, Any]]
+
+        # additional callback to be used with key handling
+        self.event_callback = None  # type: Callable
 
         self._initWindow()
         self._initCamera()
         self._initGL()
 
-        legend = '''<font face="Helvetica,Arial" size=15>wasd - move around <br>
+        legend = '''<font face="Helvetica,Arial" size=15>wasd &#8679; &#x2423; - move around <br>
         mouse drag - look <br>
         c - continous/blocking <br>
         b - add cube <br>
@@ -288,6 +294,8 @@ class Visualizer(object):
         gl.glDepthFunc(gl.GL_LESS)                 # The Type Of Depth Test To Do
         gl.glHint(gl.GL_PERSPECTIVE_CORRECTION_HINT, gl.GL_NICEST)   # make stuff look nice
         gl.glEnable(gl.GL_DEPTH_TEST)              # Enables Depth Testing
+        #gl.glEnable(gl.GL_LIGHTING)
+        #gl.glEnable(gl.GL_LIGHT0)
         #gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)   # Wireframe
 
         if not gl.glUseProgram:
@@ -377,6 +385,7 @@ class Visualizer(object):
         if symbol in [key.Q, key.ESCAPE]:
             print('leaving render')
             pyglet.app.exit()
+            return pyglet.event.EVENT_HANDLED
 
         if symbol == key.B:
             self.addBody()
@@ -386,6 +395,7 @@ class Visualizer(object):
                 print('switching to continuous render')
                 self.mode = 'c'
                 pyglet.app.exit()
+                return pyglet.event.EVENT_HANDLED
             else:
                 print('switching to blocking render')
                 self.mode = 'b'
@@ -397,6 +407,16 @@ class Visualizer(object):
         if symbol == key.R:
             print("Reset camera")
             self._initCamera()
+
+        if symbol == key.RIGHT:
+            self.display_index +=1
+            if self.event_callback:
+                self.event_callback()
+
+        if symbol == key.LEFT:
+            self.display_index -=1
+            if self.event_callback:
+                self.event_callback()
 
         '''
         if symbol in self.pressed_keys:
@@ -486,8 +506,24 @@ class Visualizer(object):
         self.bodies.append(body)
         print("Bodies: {}".format(self.bodies))
 
+    def drawCoords(self):
+        l = 0.2
+        gl.glBegin(gl.GL_LINES)
+        gl.glColor3f(1.0, 0.0, 0.0);
+        gl.glVertex3f(0.0, 0.0, 0.0)
+        gl.glVertex3f(l, 0.0, 0.0)
+
+        gl.glColor3f(0.0, 1.0, 0.0);
+        gl.glVertex3f(0.0, 0.0, 0.0)
+        gl.glVertex3f(0.0, l, 0.0)
+
+        gl.glColor3f(0.0, 0.0, 1.0);
+        gl.glVertex3f(0.0, 0.0, 0.0)
+        gl.glVertex3f(0.0, 0.0, l)
+        gl.glEnd()
+
     def drawGrid(self):
-        # dx, dy are thickness parameters of grid
+        # dx, dy are the width of grid
         xmin = -50.0
         xmax = 50.0
         dx = 5.0
@@ -530,7 +566,14 @@ class Visualizer(object):
         gl.glRotatef(np.rad2deg(y), 0.0, 0.0, 1.0)
         gl.glRotatef(np.rad2deg(-r), 1.0, 0.0, 0.0)
         gl.glRotatef(np.rad2deg(-p), 0.0, 1.0, 0.0)
+
         #gl.glMultMatrixd(glvec(trans))
+
+        self.drawCoords()
+
+        rel_pos = body['center']
+        gl.glTranslatef(rel_pos[0], rel_pos[1], rel_pos[2])
+
         if body['geometry'] is 'box':
             dim = body['boxsize']
             gl.glScalef(dim[0], dim[1], dim[2])
@@ -544,7 +587,9 @@ class Visualizer(object):
                   ignore_links    # type: List[str]                 # links that will not be drawn
                   ):
         # type: (...) -> None
-        #
+        ''' helper frunction that adds boxes for iDynTree model at position and rotations for given
+        joint angles'''
+
         if self.window_closed:
             self._initWindow()
             self._initCamera()
@@ -559,11 +604,14 @@ class Visualizer(object):
             body = {}  # type: Dict[str, Any]
             body['geometry'] = 'box'
             try:
-                b = boxes[n_name]
+                b = boxes[n_name] * self.config['scaleCollisionHull']
                 body['boxsize'] = np.array([b[0][1]-b[0][0], b[1][1]-b[1][0], b[2][1]-b[2][0]])
             except KeyError:
                 print('using cube for {}'.format(n_name))
                 body['boxsize'] = np.array([0.1, 0.1, 0.1])
+            body['center'] = 0.5*np.array([np.abs(b[0][1])-np.abs(b[0][0]),
+                                           np.abs(b[1][1])-np.abs(b[1][0]),
+                                           np.abs(b[2][1])-np.abs(b[2][0])])
             t = model.getWorldTransform(l)
             body['position'] = t.getPosition().toNumPy()
             body['rotation'] = t.getRotation().toNumPy()
@@ -592,147 +640,69 @@ class Visualizer(object):
 
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='Visualize postures or trajectories from file')
+    parser.add_argument('--config', required=True, type=str, help="use options from given config file")
+    parser.add_argument('-m', '--model', required=True, type=str, help='the file to load the robot model from')
+    parser.add_argument('--filename', required=True, type=str, help='the file to load the trajectory from')
+    args = parser.parse_args()
+
+    import yaml
+    with open(args.config, 'r') as stream:
+        try:
+            config = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
 
     import iDynTree; iDynTree.init_helpers(); iDynTree.init_numpy_helpers()
     dynComp = iDynTree.DynamicsComputations()
-    dynComp.loadRobotModelFromFile('model/walkman_measured.urdf')
+    dynComp.loadRobotModelFromFile(args.model)
     world_gravity = iDynTree.SpatialAcc.fromList([0,0,-9.81,0,0,0])
+    n_dof = dynComp.getNrOfDegreesOfFreedom()
 
-    # walkman
-    link_cuboid_hulls = {
-        'DWL':
-        np.array([[-0.11982614, 0.06288684], [-0.11, 0.121],
-                  [-0.10879193, 0.06298846]]),
-        'DWS':
-        np.array([[-0.07349821, 0.0735], [-0.0937, 0.0885],
-                  [-0.07188258, 0.2072]]),
-        'DWYTorso':
-        np.array([[-0.10220008, 0.1113662], [-0.26770911, 0.2678313],
-                  [-0.14542062, 0.07580003]]),
-        'LElb':
-        np.array([[-0.134, 0.05238984], [-0.096, 0.09307509],
-                  [-0.087, 0.05239615]]),
-        'LFoot':
-        np.array([[-0.155, 0.165], [-0.067, 0.087], [-0.1435, 0.058]]),
-        'LFootmot':
-        np.array([[-0.125, 0.055], [-0.102, 0.072], [-0.069, 0.069]]),
-        'LForearm':
-        np.array([[-0.04249971, 0.04249971], [-0.0585, 0.0585],
-                  [-0.03929598, 0.1215]]),
-        'LHipMot':
-        np.array([[-2.36900101e-01, 1.63424829e-16],
-                  [-4.74997139e-02, 1.83532211e-01],
-                  [-1.46772430e-01, 4.74721031e-02]]),
-        'LLowLeg':
-        np.array([[-0.11836108, 0.12498866], [-0.102, 0.127],
-                  [-0.43789868, 0.09436713]]),
-        'LShp':
-        np.array([[-0.105, 0.104], [-0.05777887, 0.16954893],
-                  [-0.06331461, 0.08662518]]),
-        'LShr':
-        np.array([[-0.0905, 0.0832], [-0.06499635, 0.06499484],
-                  [-0.22170009, 0.06495815]]),
-        'LShy':
-        np.array([[-0.06052907, 0.09845684], [-0.08919501, 0.08377501],
-                  [-0.21249971, 0.008]]),
-        'LSoftHandLink':
-        np.array([[-0.0980275, 0.1958], [-0.05189596, 0.0325],
-                  [-0.34175, -0.0675]]),
-        'LThighLowLeg':
-        np.array([[-0.08745305, 0.11749961], [-0.1025, 0.127],
-                  [-0.44349982, 0.06499506]]),
-        'LThighUpLeg':
-        np.array([[-0.082, 0.0655], [-0.086, 0.095], [-0.0655, 0.07949912]]),
-        'LWrMot2':
-        np.array([[-0.05528355, 0.0549846], [-0.05776, 0.0595],
-                  [-0.13198608, 0.02437494]]),
-        'LWrMot3':
-        np.array([[-0.0542625, 0.063], [-0.02866348, 0.02866348],
-                  [-0.0695, 0.025]]),
-        'NeckPitch':
-        np.array([[-0.06515144, 0.0225], [-0.07, 0.07], [-0.0225, 0.0985]]),
-        'NeckYaw':
-        np.array([[-0.0295, 0.0295], [-0.03449, 0.0295], [0.08, 0.142]]),
-        'RElb':
-        np.array([[-0.134, 0.05238984], [-0.09307509, 0.096],
-                  [-0.087, 0.05239615]]),
-        'RFoot':
-        np.array([[-0.155, 0.165], [-0.087, 0.067], [-0.1435, 0.058]]),
-        'RFootmot':
-        np.array([[-0.125, 0.055], [-0.072, 0.102], [-0.069, 0.069]]),
-        'RForearm':
-        np.array([[-0.04249971, 0.04249971], [-0.0585, 0.0585],
-                  [-0.03929598, 0.1215]]),
-        'RHipMot':
-        np.array([[-2.36900101e-01, 1.63424829e-16],
-                  [-1.83532211e-01, 4.74997139e-02],
-                  [-1.46772430e-01, 4.74721031e-02]]),
-        'RLowLeg':
-        np.array([[-0.11836108, 0.12498866], [-0.127, 0.102],
-                  [-0.43789868, 0.09436713]]),
-        'RShp':
-        np.array([[-0.105, 0.104], [-0.16954893, 0.05777887],
-                  [-0.06331461, 0.08662518]]),
-        'RShr':
-        np.array([[-0.0905, 0.0832], [-0.06499484, 0.06499635],
-                  [-0.22170009, 0.06495815]]),
-        'RShy':
-        np.array([[-0.06052907, 0.09845684], [-0.08377501, 0.08919501],
-                  [-0.21249971, 0.008]]),
-        'RSoftHandLink':
-        np.array([[-0.0980275, 0.1958], [-0.0325, 0.05189596],
-                  [-0.34175, -0.0675]]),
-        'RThighLowLeg':
-        np.array([[-0.08745305, 0.11749961], [-0.127, 0.1025],
-                  [-0.44349982, 0.06499506]]),
-        'RThighUpLeg':
-        np.array([[-0.082, 0.0655], [-0.095, 0.086], [-0.0655, 0.07949912]]),
-        'RWrMot2':
-        np.array([[-0.05528355, 0.0549846], [-0.0595, 0.05776],
-                  [-0.13198608, 0.02437494]]),
-        'RWrMot3':
-        np.array([[-0.0542625, 0.063], [-0.02866348, 0.02866348],
-                  [-0.0695, 0.025]]),
-        'TorsoProtections':
-        np.array([[-0.355, 0.25098981], [-0.21364139, 0.21364139],
-                  [-0.1705, 0.3865]]),
-        'Waist': np.array([[-0.23678584, 0.19874428], [-0.12499924, 0.12499924],
-                           [-0.11371131, 0.20991649]]),
-        'backpack': np.array([[-0.1, 0.1], [-0.1, 0.1], [-0.165, 0.035]]),
-        'imu_link': np.array([[-0.1, 0.1], [-0.1, 0.1], [-0.1, 0.1]]),
-        'imu_link2': np.array([[-0.1, 0.1], [-0.1, 0.1], [-0.1, 0.1]]),
-        'multisense/head':
-        np.array([[-0.22704816, 0.03310263], [-0.09927974, 0.09928905],
-                  [-0.06626804, 0.16240472]]),
-        'multisense/head_imu_link':
-        np.array([[-0.222993, -0.022993], [-0.0649666, 0.1350334],
-                  [-0.07226, 0.12774]]),
-        'multisense/hokuyo_link':
-        np.array([[-0.03388125, 0.03853858], [-0.04238125, 0.05379988],
-                  [-0.02115, 0.05885]])
-    }
 
-    link_names = ['Waist', 'LHipMot', 'LThighUpLeg', 'LThighLowLeg', 'LLowLeg', 'LFootmot', 'LFoot',
+    '''
+    # TODO: get this from generator / model class (other order than dynComp)
+    linkNames = ['Waist', 'LHipMot', 'LThighUpLeg', 'LThighLowLeg', 'LLowLeg', 'LFootmot', 'LFoot',
                 'RHipMot', 'RThighUpLeg', 'RThighLowLeg', 'RLowLeg', 'RFootmot', 'RFoot', 'DWL', 'DWS',
                 'DWYTorso', 'LShp', 'LShr', 'LShy', 'LElb', 'LForearm', 'LWrMot2', 'LWrMot3',
                 'LSoftHandLink', 'NeckYaw', 'NeckPitch', 'multisense/head', 'multisense/head_imu_link',
                 'multisense/hokuyo_link', 'RShp', 'RShr', 'RShy', 'RElb', 'RForearm', 'RWrMot2',
                 'RWrMot3', 'RSoftHandLink', 'backpack', 'TorsoProtections', 'imu_link', 'imu_link2']
+    '''
+    # kuka
+    linkNames = ['lwr_base_link', 'lwr_1_link', 'lwr_2_link', 'lwr_3_link', 'lwr_4_link',
+                 'lwr_5_link', 'lwr_6_link', 'lwr_7_link']
 
-    v = Visualizer()
-    n_dof = 28
-    #q0 = [0.0]*n_dof
-    q0 = np.deg2rad([0., 0, -70., 90., -20.,   0.,
-          0.,   0., -70.,  90., -20.,   0.,
-          0.,   0.,
-          0.,  10.,   0.,   0.,   0.,   0.,   0.,
-          0., -10.,   0.,   0.,   0.,   0.,   0.])
-    dq = iDynTree.VectorDynSize.fromList([0.0]*n_dof)
+    # get bounding boxes for model
+    from identification.helpers import URDFHelpers, ParamHelpers
+    paramHelpers = ParamHelpers(None, config)
+    urdfHelpers = URDFHelpers(paramHelpers, None, config)
 
-    ignore_links = ['imu_link', 'imu_link2', 'multisense/head_imu_link', 'TorsoProtections']
-    for n in np.arange(0, np.pi, 0.01):
-        q0[15] = n
+    link_cuboid_hulls = {}  # type: Dict[str, np.ndarray]
+    for i in range(len(linkNames)):
+        link_name = linkNames[i]
+        link_cuboid_hulls[link_name] = np.array(
+            urdfHelpers.getBoundingBox(
+                input_urdf = args.model,
+                old_com = [0,0,0],  # not important if proper hulls exist, only used for fallback cubes
+                link_name = link_name
+            )
+        )
+
+    v = Visualizer(config)
+
+    if config['useStaticTrajectories']:
+        data = np.load(args.filename, encoding='latin1')
+
+    def draw_model():
+        print('posture {}'.format(v.display_index))
+        q0 = data['angles'][v.display_index]['angles']
+        dq = iDynTree.VectorDynSize.fromList([0.0]*n_dof)
         q = iDynTree.VectorDynSize.fromList(q0)
         dynComp.setRobotState(q, dq, dq, world_gravity)
-        v.addIDynTreeModel(dynComp, link_cuboid_hulls, link_names, ignore_links)
-        v.run()
+        v.addIDynTreeModel(dynComp, link_cuboid_hulls, linkNames, config['ignoreLinksForCollision'])
+
+    v.event_callback = draw_model
+    v.event_callback()
+    v.run()

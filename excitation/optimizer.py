fix number of constraints calculation, some type annotations

excitation/optimizer.py
@@ -25,6 +25,7 @@ except:
     parallel = False
 
 def plotter(config, data=None, filename=None):
+    #type: (Dict, np._ArrayLike, str) -> None
     fig = plt.figure(1)
     fig.clear()
     if False:
@@ -125,21 +126,21 @@ def plotter(config, data=None, filename=None):
     d = 0
     cols = 2.0
     rows = round(len(datasets)/cols)
-    for (data, title) in datasets:
+    for (dat, title) in datasets:
         plt.subplot(rows, cols, d+1)
         plt.title(title)
         lines = list()
         labels = list()
-        for d_i in range(0, len(data)):
-            if len(data[d_i].shape) > 1:
+        for d_i in range(0, len(dat)):
+            if len(dat[d_i].shape) > 1:
                 for i in range(0, config['num_dofs']):
                     l = config['jointNames'][i] if d_i == 0 else ''  #only put joint names in the legend once
                     labels.append(l)
-                    line = plt.plot(T, data[d_i][:, i], color=colors[i], alpha=1-(d_i/2.0))
+                    line = plt.plot(T, dat[d_i][:, i], color=colors[i], alpha=1-(d_i/2.0))
                     lines.append(line[0])
             else:
-                #data vector
-                plt.plot(T, data[d_i], label=title, color=colors[0], alpha=1-(d_i/2.0))
+                #dat vector
+                plt.plot(T, dat[d_i], label=title, color=colors[0], alpha=1-(d_i/2.0))
         d+=1
     leg = plt.figlegend(lines, labels, 'upper right', fancybox=True, fontsize=10)
     leg.draggable()
@@ -152,7 +153,7 @@ def plotter(config, data=None, filename=None):
 
 class Optimizer(object):
     def __init__(self, config, model, simulation_func):
-        # type: (Dict, Model, Callable[[Dict, Trajectory, Model, np.ndarray], Tuple[Dict, Data]]) -> None
+        # type: (Dict, Model, Callable[[Dict, Trajectory, Model, np._ArrayLike], Tuple[Dict, Data]]) -> None
         self.config = config
         self.sim_func = simulation_func
         self.model = model
@@ -176,17 +177,18 @@ class Optimizer(object):
             self.initGraph()
 
         self.local_iter_max = "(unknown)"
+        self.is_global = False
 
     def testBounds(self, x):
-        # type: (np.ndarray) -> bool
+        # type: (np._ArrayLike) -> bool
         raise NotImplementedError
 
     def testConstraints(self, g):
-        # type: (np.ndarray) -> bool
+        # type: (np._ArrayLike) -> bool
         raise NotImplementedError
 
     def objectiveFunc(self, x, test=False):
-        # type: (np.ndarray[float]) -> Tuple[float, np.ndarray, bool]
+        # type: (np._ArrayLike[float], bool) -> Tuple[float, np._ArrayLike, bool]
         ''' calculate objective function and return objective function value f, constraint values g
         and a fail flag'''
         raise NotImplementedError
@@ -250,7 +252,7 @@ class Optimizer(object):
 
 
     def runOptimizer(self, opt_prob):
-        # type: (pyOpt.Optimization) -> np.ndarray[float]
+        # type: (pyOpt.Optimization) -> np._ArrayLike[float]
         ''' call global followed by local optimizer, return solution '''
 
         initial = [v.value for v in list(opt_prob.getVarSet().values())]
@@ -304,6 +306,7 @@ class Optimizer(object):
 
             if self.config['verbose']:
                 print('Running global optimization with {}'.format(self.config['globalSolver']))
+            self.is_global = True
             opt(opt_prob, store_hst=False) #, xstart=initial)
 
             if self.mpi_rank == 0:
@@ -348,6 +351,7 @@ class Optimizer(object):
 
             if self.config['verbose']:
                 print('Running local optimization with {}'.format(self.config['localSolver']))
+            self.is_global = False
             if parallel:
                 opt2(opt_prob, sens_step=0.1, sens_mode='pgc', store_hst=False)
             else:
@@ -373,5 +377,4 @@ class Optimizer(object):
         else:
             # parallel sub-processes, close
             sys.exit(0)
-
 

excitation/trajectoryGenerator.py
@@ -12,7 +12,7 @@ from identification.data import Data
 
 
 def simulateTrajectory(config, trajectory, model=None, measurements=None):
-    # type: (Dict, Trajectory, Model, np.ndarray) -> Tuple[Dict, Data]
+    # type: (Dict, Trajectory, Model, np._ArrayLike) -> Tuple[Dict, Data]
     # generate data arrays for simulation and regressor building
     old_sim = config['simulateTorques']
     config['simulateTorques'] = True
@@ -30,7 +30,7 @@ def simulateTrajectory(config, trajectory, model=None, measurements=None):
         model = Model(config, urdf)
 
     data = Data(config)
-    trajectory_data = {}   # type: Dict[str, Union[List, np.ndarray]]
+    trajectory_data = {}   # type: Dict[str, Union[List, np._ArrayLike]]
     trajectory_data['target_positions'] = []
     trajectory_data['target_velocities'] = []
     trajectory_data['target_accelerations'] = []

excite.py
@@ -47,7 +47,7 @@ from excitation.optimizer import plotter
 from excitation.trajectoryGenerator import PulsedTrajectory, FixedPositionTrajectory
 from excitation.trajectoryOptimizer import simulateTrajectory
 
-traj_data = {}   # type: Dict[str, np.ndarray]   # hold some global data vars in here
+traj_data = {}   # type: Dict[str, np._ArrayLike]   # hold some global data vars in here
 
 def main():
     if args.trajectory:

identification/data.py
@@ -21,10 +21,10 @@ class Data(object):
     def __init__(self, opt):
         # type: (Dict[str, Any]) -> None
         self.opt = opt
-        self.measurements = {}  # type: Dict[str, np.ndarray]   #loaded data
+        self.measurements = {}  # type: Dict[str, np._ArrayLike]   #loaded data
         self.num_loaded_samples = 0    # no of samples from file
         self.num_used_samples = 0      # no of samples after skipping
-        self.samples = {}     # type: Dict[str, np.ndarray]   #selected data (when using block selection)
+        self.samples = {}     # type: Dict[str, np._ArrayLike]   #selected data (when using block selection)
 
         self.usedBlocks = list()     # type: List[int]
         self.unusedBlocks = list()   # type: List[int]
@@ -34,7 +34,7 @@ class Data(object):
         self.inited = False
 
     def init_from_data(self, data):
-        # type: (Dict[str, np.ndarray]) -> None
+        # type: (Dict[str, np._ArrayLike]) -> None
         '''load data from numpy array'''
 
         self.samples = self.measurements = data.copy()

identification/helpers.py
@@ -25,7 +25,7 @@ if (sys.version_info < (3, 0)):
         pass
 
 def getNRMSE(data_ref, data_est, normalize=True, limits=None):
-    # type: (np.ndarray, np.ndarray, bool, np.ndarray) -> np.ndarray[float]
+    # type: (np._ArrayLike, np._ArrayLike, bool, np._ArrayLike) -> np._ArrayLike[float]
     '''get (normalized) root mean square error between estimated values and "standard".
     if limits is supplied, normalization is done from maximum range of torques rather than observed
     range in the data '''
@@ -113,7 +113,7 @@ class ParamHelpers(object):
         self.opt = opt
 
     def checkPhysicalConsistency(self, params, full=False):
-        # type: (np.ndarray, bool) -> (Dict[int, bool])
+        # type: (np._ArrayLike, bool) -> (Dict[int, bool])
         """
         check params for physical consistency
         (mass positive, inertia tensor positive definite, triangle inequaltiy for eigenvalues of inertia tensor expressed at COM)
@@ -140,7 +140,7 @@ class ParamHelpers(object):
         return cons
 
     def checkPhysicalConsistencyNoTriangle(self, params, full=False):
-        # type: (np.ndarray, bool) -> (Dict[int, bool])
+        # type: (np._ArrayLike, bool) -> (Dict[int, bool])
         """
         check params for physical consistency
         (mass positive, inertia tensor positive definite)
@@ -183,12 +183,12 @@ class ParamHelpers(object):
         return cons
 
     def isPhysicalConsistent(self, params):
-        # type: (np.ndarray[float]) -> bool
+        # type: (np._ArrayLike[float]) -> bool
         """give boolean consistency statement for a set of parameters"""
         return not (False in self.checkPhysicalConsistencyNoTriangle(params).values())
 
     def invvech(self, params):
-        # type: (np.ndarray[float]) -> (np.ndarray[float])
+        # type: (np._ArrayLike[float]) -> (np._ArrayLike[float])
         """give full inertia tensor from vectorized form
            expect vector of 6 values (xx, xy, xz, yy, yz, zz).T"""
         tensor = np.zeros((3,3))
@@ -216,7 +216,7 @@ class ParamHelpers(object):
         return tensor
 
     def vech(self, params):
-        # type: (np.ndarray[float]) -> (np.ndarray[float])
+        # type: (np._ArrayLike[float]) -> (np._ArrayLike[float])
         """return vectorization of symmetric 3x3 matrix (only up to diagonal)"""
         vec = np.zeros(6)
         vec[0] = params[0,0]
@@ -228,7 +228,7 @@ class ParamHelpers(object):
         return vec
 
     def inertiaTensorFromParams(self, params):
-        # type: (np.ndarray[float]) -> (List[np.ndarray[float]])
+        # type: (np._ArrayLike[float]) -> (List[np._ArrayLike[float]])
         """take a parameter vector and return list of full inertia tensors (one for each link)"""
         tensors = list()
         for i in range(len(params)):
@@ -238,7 +238,7 @@ class ParamHelpers(object):
         return tensors
 
     def inertiaParams2RotationalInertiaRaw(self, params):
-        # type: (np.ndarray[float]) -> (np.ndarray[float])
+        # type: (np._ArrayLike[float]) -> (np._ArrayLike[float])
         """take values from inertia parameter vector and create iDynTree RotationalInertiaRaw matrix
         expects six parameter vector"""
 
@@ -267,7 +267,7 @@ class ParamHelpers(object):
         return inertia
 
     def paramsLink2Bary(self, params):
-        # type: (np.ndarray[float]) -> (np.ndarray[float])
+        # type: (np._ArrayLike[float]) -> (np._ArrayLike[float])
         """convert params from iDynTree values (relative to link frame) to barycentric parameters
            (usable in URDF) (changed in place)"""
 
@@ -303,7 +303,7 @@ class ParamHelpers(object):
         return params
 
     def paramsBary2Link(self, params):
-        # type: (np.ndarray[float]) -> (np.ndarray[float])
+        # type: (np._ArrayLike[float]) -> (np._ArrayLike[float])
         params = params.copy()
         for i in range(0, len(params)):
             if (i % 10 == 0) and i < self.model.num_model_params:   #for each link
@@ -333,7 +333,7 @@ class ParamHelpers(object):
 
     @staticmethod
     def addFrictionFromURDF(model, urdf_file, params):
-        # type: (model.Model, str, np.ndarray[float]) -> None
+        # type: (model.Model, str, np._ArrayLike[float]) -> None
         ''' get friction vals from urdf (joint friction = fc, damping= fv) and set in params vector'''
 
         friction = URDFHelpers.getJointFriction(urdf_file)
@@ -371,7 +371,7 @@ class URDFHelpers(object):
         return tree
 
     def replaceParamsInURDF(self, input_urdf, output_urdf, new_params):
-        # type: (str, str, np.ndarray[float]) -> None
+        # type: (str, str, np._ArrayLike[float]) -> None
         """ set new inertia parameters from params and urdf_file, write to new temp file """
 
         if self.opt['identifyGravityParamsOnly']:

identification/sdp.py
@@ -44,7 +44,7 @@ class SDP(object):
 
 
     def checkFeasibility(self, prime):
-        # type: (np.ndarray) -> bool
+        # type: (np._ArrayLike) -> bool
         ''' check for a given parameter vector, e.g. a starting point, if it is within the LMI
         constraints '''
 
@@ -650,7 +650,7 @@ class SDP(object):
             DB_blocks = [self.mrepl(Di, self.varchange_dict) for Di in self.D_blocks]
             self.DB_LMIs_marg = list([LMI_PSD(lm - self.epsilon_safemargin*eye(lm.shape[0])) for lm in DB_blocks])
 
-            Q, R = la.qr(idf.model.YBase)  # type: (np.ndarray[float], np.ndarray[float])
+            Q, R = la.qr(idf.model.YBase)
             #Q1 = Q[:, 0:idf.model.num_base_params]
             #Q2 = Q[:, idf.model.num_base_params:]
             R1 = np.matrix(R[:idf.model.num_base_params, :idf.model.num_base_params])  # type: np.matrix[float]
@@ -705,7 +705,7 @@ class SDP(object):
 
 
     def findFeasibleStdFromFeasibleBase(self, idf, xBase):
-        # type: (Identification, np.ndarray) -> None
+        # type: (Identification, np._ArrayLike) -> None
         ''' find a std feasible solution for feasible base solution (exists by definition) while
             minimizing param distance to a-priori parameters
         '''
@@ -775,7 +775,7 @@ class SDP(object):
             print("Constrained SDP optimization took %.03f sec." % (t.interval))
 
     def findFeasibleStdFromStd(self, idf, xStd):
-        # type: (Identification, np.ndarray) -> (np.ndarray)
+        # type: (Identification, np._ArrayLike) -> (np._ArrayLike)
         ''' find closest feasible std solution for some std parameters (increases error) '''
 
         idable_params = sorted(list(set(idf.model.identified_params).difference(self.delete_cols)))

identify.py
@@ -84,8 +84,8 @@ class Identification(object):
         self.urdfHelpers = helpers.URDFHelpers(self.paramHelpers, self.model, self.opt)
         self.sdp = sdp.SDP(self)
 
-        self.tauEstimated = None  # type: np.ndarray
-        self.res_error = 100        #last residual error in percent
+        self.tauEstimated = None    # type: np._ArrayLike
+        self.res_error = 100        # last residual error in percent
 
         self.urdf_file_real = urdf_file_real
         if self.urdf_file_real:
@@ -230,7 +230,7 @@ class Identification(object):
         old_skip = self.opt['skipSamples']
         self.opt['skipSamples'] = 8
 
-        self.tauEstimatedValidation = None   # type: np.ndarray
+        self.tauEstimatedValidation = None   # type: np._ArrayLike
         for m_idx in self.progress(range(0, v_data['positions'].shape[0], self.opt['skipSamples'] + 1)):
             if self.opt['useRBDL']:
                 torques = self.model.simulateDynamicsRBDL(v_data, m_idx, params)
@@ -298,7 +298,7 @@ class Identification(object):
 
 
     def getStdDevForParams(self):
-        # type: () -> (np.ndarray[float])
+        # type: () -> (np._ArrayLike[float])
         # this might not be working correctly
         if self.opt['useAPriori']:
             tauDiff = self.model.tauMeasured - self.tauEstimated
@@ -557,7 +557,7 @@ class Identification(object):
 
 
     def identifyBaseParameters(self, YBase=None, tau=None, id_only=False):
-        # type: (np.ndarray, np.ndarray, bool) -> None
+        # type: (np._ArrayLike, np._ArrayLike, bool) -> None
         """use previously computed regressors and identify base parameter vector using ordinary or
            weighted least squares."""
 